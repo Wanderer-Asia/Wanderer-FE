bcbe167fff54c7a551dfdd7cb78f92004d46bede
Arif Mamad <marifrahmawan@gmail.com>
2023-12-16 14:02:47 +0800

    dashboard page

--- src/components/Admin/Sidebar.tsx
+++ src/components/Admin/Sidebar.tsx
@@ -1,6 +1,28 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../Logo";
-import { Banknote, LayoutDashboard } from "lucide-react";
+import { Banknote, LayoutDashboard, LucideIcon } from "lucide-react";
+
+type LinkListType = {
+  title: string;
+  to: string;
+  end: boolean | undefined;
+  icon: LucideIcon;
+}[];
+
+const LinkList: LinkListType = [
+  {
+    title: "Dashboard",
+    to: "/admin",
+    end: true,
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Transactions",
+    to: "transactions",
+    end: false,
+    icon: Banknote,
+  },
+];
 
 const Sidebar = () => {
   return (
@@ -10,78 +32,44 @@ const Sidebar = () => {
       </div>
 
       <div className="mt-5 flex flex-col p-3">
-        <NavLink
-          to={"/admin"}
-          className={({ isActive }) =>
-            isActive
-              ? "flex w-full items-center gap-4 rounded-lg bg-yellow-main py-4 pl-3"
-              : "flex w-full items-center gap-4 rounded-lg bg-transparent py-4 pl-3"
-          }
-          end
-        >
-          {({ isActive }) => (
-            <>
-              <div
-                className={`flex h-7 w-7 items-center justify-center rounded-lg border p-1 ${
-                  isActive
-                    ? "border-[#0F5FC2]"
-                    : "border-black dark:border-white"
-                }`}
-              >
-                <LayoutDashboard
-                  className={`h-full w-full ${
+        {LinkList.map((link) => (
+          <NavLink
+            to={link.to}
+            className={({ isActive }) =>
+              isActive
+                ? "flex w-full items-center gap-4 rounded-lg bg-yellow-main py-4 pl-3"
+                : "flex w-full items-center gap-4 rounded-lg bg-transparent py-4 pl-3"
+            }
+            end={link.end}
+          >
+            {({ isActive }) => (
+              <>
+                <div
+                  className={`flex h-7 w-7 items-center justify-center rounded-lg border p-1 ${
                     isActive
-                      ? "stroke-[#0F5FC2]"
-                      : "stroke-black dark:stroke-white"
+                      ? "border-[#0F5FC2]"
+                      : "border-black dark:border-white"
                   }`}
-                />
-              </div>
-              <p
-                className={`${
-                  isActive ? "font-bold text-[#0F5FC2]" : "font-normal"
-                }`}
-              >
-                Dashboard
-              </p>
-            </>
-          )}
-        </NavLink>
-
-        <NavLink
-          to={"transactions"}
-          className={({ isActive }) =>
-            isActive
-              ? "flex w-full items-center gap-4 rounded-lg bg-yellow-main py-4 pl-3"
-              : "flex w-full items-center gap-4 rounded-lg bg-transparent py-4 pl-3"
-          }
-        >
-          {({ isActive }) => (
-            <>
-              <div
-                className={`flex h-7 w-7 items-center justify-center rounded-lg border p-1 ${
-                  isActive
-                    ? "border-[#0F5FC2]"
-                    : "border-black dark:border-white"
-                }`}
-              >
-                <Banknote
-                  className={`h-full w-full ${
-                    isActive
-                      ? "stroke-[#0F5FC2]"
-                      : "stroke-black dark:stroke-white"
+                >
+                  <link.icon
+                    className={`h-full w-full ${
+                      isActive
+                        ? "stroke-[#0F5FC2]"
+                        : "stroke-black dark:stroke-white"
+                    }`}
+                  />
+                </div>
+                <p
+                  className={`${
+                    isActive ? "font-bold text-[#0F5FC2]" : "font-normal"
                   }`}
-                />
-              </div>
-              <p
-                className={`${
-                  isActive ? "font-bold text-[#0F5FC2]" : "font-normal"
-                }`}
-              >
-                Transactions
-              </p>
-            </>
-          )}
-        </NavLink>
+                >
+                  {link.title}
+                </p>
+              </>
+            )}
+          </NavLink>
+        ))}
       </div>
     </div>
   );
